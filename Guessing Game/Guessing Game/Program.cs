1b879307a8cfa4292fd533c0c4863f147f098b91
JazzSingh16 <thejazzsingh16@gmail.com>
2013-10-04 08:14:52 -0400

    Guessing Done

--- Guessing Game/Guessing Game/Program.cs
+++ Guessing Game/Guessing Game/Program.cs
@@ -3,12 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Guessing_Game
+namespace GuessingGame
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int guess;
+            int x = 1;
+            int y = 100;
+            string input;
+
+
+
+            Console.WriteLine("Think of a number between 1 and 100");
+            while (true)
+            {
+                guess = (x + y) / 2;
+
+                Console.WriteLine("Is the number {0}", guess);
+
+                Console.WriteLine("h, l, or e");
+
+                input = Console.ReadLine();
+                if (input == "h")
+                {
+                    x = guess + 1;
+                }
+                if (input == "l")
+                {
+                    y = guess - 1;
+                }
+                if (input == "e")
+                {
+                    break;
+                }
+
+            }
         }
     }
 }
